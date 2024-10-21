4dbfbf6f7c8645466a7f03cd3cca8e47a4e33adb
Rey Vincent S. Labuguen <labuguen.254620@baguio.sti.edu.ph>
2024-10-21 16:37:11 +0800

    second commit

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -2,7 +2,6 @@ import {startCase} from "lodash";
 
 import { OrgControl } from "./_components/org-control";
 import { auth } from "@clerk/nextjs/server";
-import { title } from "process";
 
 export async function generateMetadata() {
     const { orgSlug } = auth ();
