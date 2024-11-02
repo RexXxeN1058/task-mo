20cd5f6b6893b625229922143ec02a7e4a22bd37
Rey Vincent S. Labuguen <labuguen.254620@baguio.sti.edu.ph>
2024-11-03 04:54:18 +0800

    Fix Info

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -7,6 +7,7 @@ import Image  from "next/image";
 export const Info = () => {
     const { organization, isLoaded } = useOrganization();
 
+
     if (!isLoaded) {
         return (
             <Info.Skeleton />
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,12 @@
 import { Separator } from "@/components/ui/separator";
 import { BoardList } from "./_components/board-list";
 import { Suspense } from "react";
+import { Info } from "./_components/info";
 
 const OrganizationIdPage = async () => {
     return (
         <div className="w-full mb-20">
+            <Info/>
             <Separator className="my-4" />
             <div className="px-2 md:px-4">
                 <Suspense fallback={<BoardList.Skeleton />}>
