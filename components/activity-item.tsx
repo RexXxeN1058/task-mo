caf7da191cefb8484af3a402e225a84fda22817a
Rey Vincent S. Labuguen <labuguen.254620@baguio.sti.edu.ph>
2024-11-03 13:59:10 +0800

    manila

--- components/activity-item.tsx
+++ components/activity-item.tsx
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+import { toZonedTime } from 'date-fns-tz'; 
 import { AuditLog } from "@prisma/client";
 import { generateLogMessage } from "@/lib/generate-log-message";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
@@ -7,6 +9,9 @@ interface ActivityItemProps {
 }
 
 export const ActivityItem = ({ data }: ActivityItemProps) => {
+  const date = new Date(data.createdAt);
+  const zonedDate = toZonedTime(date, 'Asia/Manila'); // Replace 'Asia/Manila' with your desired time zone
+
   return (
     <li className="flex items-center gap-x-2">
       <Avatar className="h-8 w-8">
@@ -20,26 +25,9 @@ export const ActivityItem = ({ data }: ActivityItemProps) => {
           {generateLogMessage(data)}
         </p>
         <p className="text-xs text-muted-foreground">
-          {formatTime(new Date(data.createdAt))} 
+          {format(zonedDate, "MMM d, yyyy 'at' h:mm a")} 
         </p>
       </div>
     </li>
   );
 };
-
-export default function formatTime(dateInput: Date) {
-  const date = new Date(dateInput);
-  const localDate = new Date(date.getTime() + date.getTimezoneOffset() * 60000);
-
-  const dayOfMonth = localDate.getDate();
-  const month = localDate.toLocaleString("en", { month: "short" });
-  const year = localDate.getFullYear();
-  let hours = localDate.getHours();
-  const minutes = localDate.getMinutes();
-  const ampm = hours >= 12 ? "pm" : "am";
-  hours = hours % 12;
-  hours = hours ? hours : 12; 
-  const minutesStr = minutes < 10 ? '0' + minutes : minutes;
-
-  return `${month} ${dayOfMonth}, ${year} at ${hours}:${minutesStr} ${ampm}`;
-}
